945f9988c79f6babf1c26f4b863e0ae3bb6dd007
nelsonjm <njoeden05@gmail.com>
2021-11-03 10:01:32 -0700

    hello

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log('hello im alive');
+
+document.write("<h1> loops demo</h1>");
+
+function while1(){ 
+let i=0;
+while(i<=5){
+    console.log(i*2+1);
+    i++;
+}
+}
+function while2(){ 
+let i=0;
+while(i<=5){
+    console.log(i*2)
+    i++;
+}
+}
+console.log(while1());
+console.log(while2());
+
+function for1(){ 
+for(i=0; -<12 i=i+1){
+    console.log(i*8);
+}
+}
+console.log(for1())
